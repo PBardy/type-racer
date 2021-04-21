f110b559642e98c8445f041a16310533638de76a
PhilipBardy <pmb44@bath.ac.uk>
2021-04-22 00:34:22 +0100

    fixed netlify issue

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import HomeView from "../views/home";
 import RaceView from "../views/race";
 
 export default class SecureRouter extends React.Component {
